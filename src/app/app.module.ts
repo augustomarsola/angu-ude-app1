5a4423b28e6d54bf72626a860cbb33c1ec8e6fc0
Augusto Marsola <augusto.marsola@live.com>
2023-12-09 18:08:53 -0300

    Added the painel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { TopoComponent } from "./topo/topo.component";
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
     declarations: [
       AppComponent,
-      TopoComponent
+      TopoComponent,
+      PainelComponent
     ],
   imports: [
     BrowserModule,
