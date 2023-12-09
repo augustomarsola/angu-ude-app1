0c8c74c5e5b58c921f1f580b6610d296af17bfee
Augusto Marsola <augusto.marsola@live.com>
2023-12-09 18:16:01 -0300

    Added progresso and tentativas components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { TopoComponent } from "./topo/topo.component";
 import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
     declarations: [
       AppComponent,
       TopoComponent,
-      PainelComponent
+      PainelComponent,
+      TentativasComponent,
+      ProgressoComponent
     ],
   imports: [
     BrowserModule,
