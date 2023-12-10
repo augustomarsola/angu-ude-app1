0ee06c3710a2873c27dcb637e8ad001d55fa8a26
Augusto Marsola <augusto.marsola@live.com>
2023-12-10 15:08:10 -0300

    Added string interpolation

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  // styleUrls: './topo.component.scss',
 })
-export class TopoComponent {}
+export class TopoComponent {
+  public titulo: string = 'Aprendendo Inglês';
+}
