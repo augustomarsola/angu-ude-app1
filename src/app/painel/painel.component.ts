669072f1d74d1a7f81c7d59c7428e235d472c580
Augusto Marsola <augusto.marsola@live.com>
2023-12-10 16:49:57 -0300

    Added checkAnswer method

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -8,10 +8,27 @@ import { FRASES } from './frases-mock';
 })
 export class PainelComponent {
   public prases: Frase[] = FRASES;
-  public instruction: string = 'Traduza a frase:'
+  public instruction: string = 'Traduza a frase:';
   public answer: string = '';
+
+  public round: number = 0;
+  public praseRound: Frase = {} as Frase;
+
+  constructor() {
+    this.praseRound = this.prases[this.round];
+  }
 
   public updateAnswer(answer: Event): void {
     this.answer = (<HTMLInputElement>answer.target).value;
+  }
+
+  public checkAnswer(): void {
+    if (this.answer === this.praseRound.frasePt) {
+      alert('Acertou!');
+      this.round++;
+      this.praseRound = this.prases[this.round];
+    } else {
+      alert('Errou!');
+    }
   }
 }
