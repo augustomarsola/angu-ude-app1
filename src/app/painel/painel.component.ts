54e48402421b43cd16029fd9dc466ebbef9a34a1
Augusto Marsola <augusto.marsola@live.com>
2023-12-10 16:25:37 -0300

    Added event binding to painel component

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
 
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
 })
 export class PainelComponent {
+  public prases: Frase[] = FRASES;
+  public instruction: string = 'Traduza a frase:'
+  public answer: string = '';
 
+  public updateAnswer(answer: Event): void {
+    this.answer = (<HTMLInputElement>answer.target).value;
+  }
 }
